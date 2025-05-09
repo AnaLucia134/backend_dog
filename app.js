c6b1ba4505116e4f16538fdb90492c76bf3e1a82
AnaLucia134 <analuperezal@gmail.com>
2025-05-09 05:13:02 +0000

    Add Jenkinsfile for CI/CD

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const app = express();
 const dogRoutes = require('./routes/dogRoutes');
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3001', 'http://192.241.148.118:3001'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/api/dogs', dogRoutes);
 
 const PORT = 3000;
